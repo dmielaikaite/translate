f5b4ed3701d391f50fb9e02ebfb6a2ea36fb0378
Donata Mielaikaite <d.mielaikaite@gmail.com>
2019-05-05 14:44:34 +0200

    steps how to add context object

--- src/components/button.js
+++ src/components/button.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
+import LanguageContext from '../contexts/languageContext';
+
+// create contect object
+//define the contextType
+//use the context
 
 class Button extends Component {
+  static contextType = LanguageContext;
+
   render(){
+    console.log(this.context);
     return(
       <button className="ui button primary"> Submit </button>
     );
